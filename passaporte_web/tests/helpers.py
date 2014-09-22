dcf089f94e40ff6df96d44bee4c1dd018ccd00f4
Vitor M. A. da Cruz <vitor.mazzi@myfreecomm.com.br>
2014-09-22 18:55:29 -0300

    Atualizando vcrpy para 1.1.0
    Atualizando formato dos cassettes

--- passaporte_web/tests/helpers.py
+++ passaporte_web/tests/helpers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
-from vcr import VCR                                                  
+from vcr import VCR
 
 __all__ = ['use_cassette']
 
@@ -8,7 +8,7 @@ def use_cassette(*args, **kwargs):
     return VCR(
         cassette_library_dir = os.path.join(os.path.dirname(__file__), 'cassettes', 'passaporte_web'),
         match_on = ['url', 'method', 'headers', 'body'],
-        record_mode = 'new_episodes',
+        record_mode = 'none',
     ).use_cassette(*args, **kwargs)
 
 
